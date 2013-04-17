a6e32bd3f1269373fd26d57283ed8ebb984bcc7e
Raghu Betina <rbetina@gmail.com>
2013-04-17 11:02:48 -0500

    Refactored to class method.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,22 +1,6 @@
-require 'open-uri'
-
 class StudentsController < ApplicationController
   def section
-    url = "http://yearbook-api.herokuapp.com/2013/Spring/37.json"
-    raw_response = open(url).read
-    result = JSON.parse(raw_response)
-    list_from_api = result["students"]
-
-    @students = Array.new
-
-    list_from_api.each do |student_hash|
-      s = Student.new
-      s.name = student_hash["first_name"] + " " + student_hash["last_name"]
-      s.photo_url = student_hash["avatar"]
-      s.section = "BegHTML"
-      s.twitter = student_hash["twitter"]
-
-      @students << s
-    end
+    @course_number = params[:id_number]
+    @students = Student.fetch(@course_number)
   end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,25 @@
+require 'open-uri'
+
 class Student
   attr_accessor :twitter, :section, :photo_url, :name
+
+  def Student.fetch(course_id)
+    url = "http://yearbook-api.herokuapp.com/2013/Spring/#{course_id}.json"
+    raw_response = open(url).read
+    result = JSON.parse(raw_response)
+    list_from_api = result["students"]
+
+    array_of_students = Array.new
+
+    list_from_api.each do |student_hash|
+      s = Student.new
+      s.name = student_hash["first_name"] + " " + student_hash["last_name"]
+      s.photo_url = student_hash["avatar"]
+      s.section = "BegHTML"
+      s.twitter = student_hash["twitter"]
+
+      array_of_students << s
+    end
+    return array_of_students
+  end
 end
